491b860145153f4b0d4357ad90e4aa946d9b622c
providence <jiminycricketsolutions@gmail.com>
2023-03-23 08:32:46 -0500

    Completed Day 1 Band name generator project

--- Days/day1.py
+++ Days/day1.py
@@ -2,3 +2,6 @@
 
 print("Welcome to the Band Name Generator.")
 print("What's the name of the city you grew up in?")
+city = input("")
+pet = input("What is the name of your pet?\n")
+print("Your band name is " + city + " " + pet + "!")
